866d4dcdfa456acedaef1993a8cf84c14339e2c9
pwoosam <pwoosam5@icloud.com>
2026-02-01 18:38:48 -0800

    Made dataframe containing all data available

--- notebooks/utils/data_loader_utils.py
+++ notebooks/utils/data_loader_utils.py
@@ -2,14 +2,6 @@
 # -*- coding: utf-8 -*-
 
 """
-
-
-
-
-
-"""
-
-""" 
 # Copyright (c) 2021 Bosch Rexroth AG
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -17,14 +9,16 @@ Author: Mohamed-Ali Tnani (Mohamed-ali.tnani@boschrexroth.de)
 Date: November 24, 2021
 
 This software is provided by the copyright holders and contributors "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. In no event shall the copyright holder or contributors be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
+
+Modified by: AAI 530 Final Project Group 5 Team Members
 """
 
 import os
-import sys
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import Literal
+
 
 def find_all_h5s_in_dir(s_dir: os.PathLike):
     """
@@ -39,12 +33,17 @@ def find_all_h5s_in_dir(s_dir: os.PathLike):
     return fileslist
 
 
-def load_tool_research_data(data_path: os.PathLike, label: Literal["good","bad"], add_additional_label: bool =True, verbose: bool =True):
+def load_tool_research_data(
+    data_path: os.PathLike,
+    label: Literal["good", "bad"],
+    add_additional_label: bool = True,
+    verbose: bool = True,
+):
     """
     load data (good and bad) from the research data storages
-    
+
     Keyword Arguments:
-            data_path {str} -- [path to the directory] 
+            data_path {str} -- [path to the directory]
             label {str} -- ["good" or "bad"]
             add_additional_label {bool} -- [if true the labels will be in the form of "Mxx_Aug20xx_OPxx_sampleNr_label" otherwise "label"] (default: True)
             verbose {bool}
@@ -66,15 +65,15 @@ def load_tool_research_data(data_path: os.PathLike, label: Literal["good","bad"]
     if verbose:
         print(f"loading files from {data_path}... ")
     for element in list_paths:
-        # check if additional label needed ("Mxx_Aug20xx_Tool,nrX") 
+        # check if additional label needed ("Mxx_Aug20xx_Tool,nrX")
         if add_additional_label:
-            add_label = element.split('/')[-1]
+            add_label = element.split("/")[-1]
             additional_label = add_label[:-3] + "_" + label
         else:
             additional_label = label
-        # extract data X and y 
-        with h5py.File(os.path.join(data_path, element), 'r') as f:
-            vibration_data = f['vibration_data'][:]
+        # extract data X and y
+        with h5py.File(os.path.join(data_path, element), "r") as f:
+            vibration_data = f["vibration_data"][:]
         datalist.append(vibration_data)
         data_label.append(additional_label)
 
@@ -85,39 +84,59 @@ def datafile_read(file: os.PathLike, plotting=True):
     """loads and plots the data from the datafile
 
     Keyword Arguments:
-        file {str} -- [path of the file] 
+        file {str} -- [path of the file]
 
     Returns:
         ndarray --  [raw data original]
     """
-    with h5py.File(file, 'r') as f:
-        vibration_data = f['vibration_data'][:]
+    with h5py.File(file, "r") as f:
+        vibration_data = f["vibration_data"][:]
+
+    # plotting
+    if plotting:
+        plot_vibration_data(vibration_data)
+
+    return vibration_data
+
+
+def plot_vibration_data(vibration_data: np.ndarray, freq: int = 2000, label: str = ""):
+    """plots the vibration data
+
+    Arguments:
+        vibration_data {ndarray} --  [raw data original]
+        label {str} -- [label for the plot]
+    Keyword Arguments:
+        freq {int} -- [sampling frequency] (default: {2000})
+    """
     # interpolation for x axis plot
-    freq = 2000
     samples_s = len(vibration_data[:, 0]) / freq
     samples = np.linspace(0, samples_s, len(vibration_data[:, 0]))
 
     # plotting
-    if plotting:
-        plt.figure(figsize=(20, 5))
-        plt.plot(samples, vibration_data[:, 0], 'b')
-        plt.ylabel('X-axis Vibration Data')
-        plt.xlabel('Time [sec]')
-        plt.locator_params(axis='y', nbins=10)
-        plt.grid()
-        plt.show()
-        plt.figure(figsize=(20, 5))
-        plt.plot(samples, vibration_data[:, 1], 'b')
-        plt.ylabel('Y-axis Vibration Data')
-        plt.xlabel('Time [sec]')
-        plt.locator_params(axis='y', nbins=10)
-        plt.grid()
-        plt.show()
-        plt.figure(figsize=(20, 5))
-        plt.plot(samples, vibration_data[:, 2], 'b')
-        plt.ylabel('Z-axis Vibration Data')
-        plt.xlabel('Time [sec]')
-        plt.locator_params(axis='y', nbins=10)
-        plt.grid()
-        plt.show()
-    return vibration_data
+    plt.figure(figsize=(20, 5))
+    plt.plot(samples, vibration_data[:, 0], "b")
+    if label:
+        plt.title(f"{label} - X-axis Vibration Data")
+    plt.ylabel("X-axis Vibration Data")
+    plt.xlabel("Time [sec]")
+    plt.locator_params(axis="y", nbins=10)
+    plt.grid()
+    plt.show()
+    plt.figure(figsize=(20, 5))
+    plt.plot(samples, vibration_data[:, 1], "b")
+    if label:
+        plt.title(f"{label} - Y-axis Vibration Data")
+    plt.ylabel("Y-axis Vibration Data")
+    plt.xlabel("Time [sec]")
+    plt.locator_params(axis="y", nbins=10)
+    plt.grid()
+    plt.show()
+    plt.figure(figsize=(20, 5))
+    plt.plot(samples, vibration_data[:, 2], "b")
+    if label:
+        plt.title(f"{label} - Z-axis Vibration Data")
+    plt.ylabel("Z-axis Vibration Data")
+    plt.xlabel("Time [sec]")
+    plt.locator_params(axis="y", nbins=10)
+    plt.grid()
+    plt.show()
